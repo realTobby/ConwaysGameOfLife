4e36d118783eb06f359106e0498ef3d1264bf289
Sillus <tkattanek00@gmail.com>
2019-05-10 10:57:13 +0200

    Add files via upload

---
+++ ConwaysGameOfLife/GOL.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    public class GOL
+    {
+
+        private bool[,] cells;
+        private int width;
+        private int height;
+        private Random rnd = new Random();
+        public int GENERATION = 0;
+
+        public GOL(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public void SetPlayfield()
+        {
+            cells = new bool[width, height];
+        }
+
+        public void ClearPlayfield()
+        {
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    cells[x, y] = false;
+                }
+            }
+        }
+
+        public void DrawPlayfield()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    switch (cells[x,y])
+                    {
+                        case true:
+                            Console.Write("█");
+                            break;
+                        case false:
+                            Console.Write(" ");
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void InitPlayfield()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int thresh = rnd.Next(0, 100);
+                    if(thresh > 90)
+                    {
+                        cells[x, y] = true;
+                    }
+                }
+            }
+        }
+
+        public void ComputeNextGeneration()
+        {
+            GENERATION++;
+            bool[,] oldGen = cells;
+
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    int neighbours = GetNeighbours(x, y);
+
+                    if(oldGen[x,y] == false)
+                    {
+                        if(neighbours == 3)
+                        {
+                            cells[x, y] = true;
+                        }
+                    }
+
+                    if(oldGen[x,y] == true)
+                    {
+                        if(neighbours < 2)
+                        {
+                            cells[x, y] = false;
+                        }
+                        if(neighbours == 2 || neighbours == 3)
+                        {
+                            cells[x, y] = true;
+                        }
+                        if(neighbours > 3)
+                        {
+                            cells[x, y] = false;
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        private int GetNeighbours(int x, int y)
+        {
+            int n = 0;
+            if (x == 0 || x == width-1 || y == 0 || y == height-1)
+            {
+                return 0;
+            }
+            
+            if(cells[x-1,y-1] == true)
+            {
+                n++;
+            }
+
+            if(cells[x,y-1] == true)
+            {
+                n++;
+            }
+
+            if(cells[x+1,y-1] == true)
+            {
+                n++;
+            }
+
+            if(cells[x-1,y] == true)
+            {
+                n++;
+            }
+
+            if(cells[x+1,y] == true)
+            {
+                n++;
+            }
+
+            if(cells[x-1,y+1] == true)
+            {
+                n++;
+            }
+
+            if(cells[x,y+1] == true)
+            {
+                n++;
+            }
+
+            if(cells[x+1,y+1] == true)
+            {
+                n++;
+            }
+
+            return n;
+        }
+
+    }
+}
---
+++ ConwaysGameOfLife/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    class Program
+    {
+        static GOL g = new GOL(50, 25);
+
+
+        static void NewPlayfield()
+        {
+            g.SetPlayfield();
+            g.ClearPlayfield();
+            g.InitPlayfield();
+        }
+
+        static void Main(string[] args)
+        {
+            NewPlayfield();
+            
+
+            while (true)
+            {
+                g.DrawPlayfield();
+                g.ComputeNextGeneration();
+                Console.Title = "GENERATION: " + g.GENERATION;
+                //Thread.Sleep(200);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
